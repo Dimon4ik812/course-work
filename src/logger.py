84d7c0c935d9bd5d7080c043d3da71b4283f1cf0
Дмитрий <kriukov@pos-credit.ru>
2024-06-22 20:07:47 +0300

    read_excel_to_dict_lict

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,15 @@
 import logging
+from typing import Any
+import os
 
-logger = logging.getLogger('save_to_file')
-logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs/save_to_file.log')
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
-file_handler.setFormatter(file_formatter)
-logger.addHandler(file_handler)
+def setup_logger(name: str, file_logs: str) -> Any:
+    """Функция логирования"""
+    os.makedirs("logs", exist_ok=True)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(file_logs, mode='w')
+    file_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
